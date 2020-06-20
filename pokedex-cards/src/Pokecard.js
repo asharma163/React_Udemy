681bb00b704dbe833ba7d018b5801bfe53350fe8
asharma163 <anshulsharma427@gmail.com>
2020-06-20 02:49:14 -0400

    Added enhanced image URL

--- pokedex-cards/src/Pokecard.js
+++ pokedex-cards/src/Pokecard.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import './Pokecard.css'
-const POKE_BASE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_BASE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let pad = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
 
 class Pokecard extends Component {
     render() {
-        let image_src = `${POKE_BASE_API}${this.props.id}.png`
+        let image_src = `${POKE_BASE_API}${pad(this.props.id)}.png`
         return (
             <div className = "Pokecard">
                 <h1 className = "Pokecard-title">{this.props.name}</h1>
